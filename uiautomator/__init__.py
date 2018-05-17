c68b6371ee8e7a0f0f390d305e7396c5ddf79a2c
Anuraag Palash Baishya <anuraagpalash.baishya@citrix.com>
2018-05-17 12:13:48 +0530

    Bug fixes

--- uiautomator/__init__.py
+++ uiautomator/__init__.py
@@ -14,16 +14,15 @@ from constants import DEVICE_PORT, LOCAL_PORT
 from automator_server import AutomatorServer
 from automator_device import device, Device, AutomatorDevice
 from automator_device_object import AutomatorDeviceObject
+from automator_device_uiobject import AutomatorDeviceUiObject
 from automator_device_named_uiobject import AutomatorDeviceNamedUiObject
-
-
 
 if 'localhost' not in os.environ.get('no_proxy', ''):
     os.environ['no_proxy'] = "localhost,%s" % os.environ.get('no_proxy', '')
 
 __author__ = "Xiaocong He"
 __all__ = ["device", "Device", "utils", "Selector", "JsonRPCError", "JsonRPCClient", "JsonRPCMethod", "Adb",
-           "AutomatorDevice", "AutomatorDeviceObject", "AutomatorDeviceNamedUiObject", "AutomatorServer"]
+           "AutomatorDevice", "AutomatorDeviceObject", "AutomatorDeviceUiObject", "AutomatorDeviceNamedUiObject", "AutomatorServer"]
 
 
 
